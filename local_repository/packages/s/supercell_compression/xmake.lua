46ecac392353c6818db7faf3e52ebe41950e780e
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2024-06-21 13:32:05 +0100

    fix xmake supercell_compression

--- local_repository/packages/s/supercell_compression/xmake.lua
+++ local_repository/packages/s/supercell_compression/xmake.lua
@@ -4,12 +4,23 @@ package("supercell_compression")
     set_license("MIT")
 
     add_urls("https://github.com/Chi-EEE/SupercellCompression.git")
-    add_versions("2024.06.21", "b358bc2f3ab595a3b0dcf14675b423b3fe15cb42")
+    add_versions("2024.06.21", "e8ebde7f1cd8abb7ef3653cb617471aa3d6eb5d6")
 
-    add_deps("lzham_codec", "lzma", "zstd", "astc-encoder")
+    add_deps("lzham_codec", "lzma", "zstd")
+    add_deps("astc-encoder", {configs = {sse41 = true, native = true, cli = false}})
     add_deps("supercell_core")
 
     on_install(function (package)
         local configs = {}
         import("package.tools.xmake").install(package, configs)
+    end)
+
+    on_test(function (package)
+        assert(package:check_cxxsnippets({test = [[
+            #include <SupercellCompression.h>
+            void test() {
+                sc::Decompressor::Lzham::Props props;
+                sc::Decompressor::Lzham context(props);
+            }
+        ]]}, {configs = {languages = "c++17"}}))
     end)
